d848c7631e393eaba3c8edf5949cdb3af6bd7fbf
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-02-19 18:21:45 +0700

    Update Calculator class

--- calculator_controller.py
+++ calculator_controller.py
@@ -10,30 +10,34 @@ class Calculator:
         """Initialize the Calculator."""
         self.snapshot_manager = SnapshotManager()
 
-    def calculate(self, equation: str) -> str:
+    def calculate(self, equation: str, last_equation: list[str]) -> str:
         """Calculate the result of the equation.
 
         :param equation: The equation to calculate.
-        :return: The result of the calculation."""
+        :param last_equation: The list of the last equations.
+        :return: The result of the calculation.
+        """
         try:
             pattern = r"\(([^)]+)\)!"
             formatted_equation = re.sub(pattern, lambda match: f"factorial({match.group(1)})", equation)
             result = eval(formatted_equation.replace("^", "**"))
-            self.save_snapshot(equation, result)
+            self.save_snapshot(equation, result, last_equation)
             return result
         except Exception:
             return "Error"
 
-    def save_snapshot(self, equation: str, result: str) -> None:
+    def save_snapshot(self, equation: str, result: str, last_equation: list[str]) -> None:
         """
         Save a snapshot of the equation and result.
 
         :param equation: The equation.
         :param result: The result of the equation.
-        """
-        self.snapshot_manager.add_new_snapshot(equation, result)
+        :param last_equation: The list of the last equations.
 
-    def load_snapshot(self) -> list[str]:
+        """
+        self.snapshot_manager.add_new_snapshot(equation, result, last_equation)
+
+    def load_snapshot(self) -> list[tuple[str, list[str]]]:
         """Load all snapshots.
 
         :return: A list of snapshot equations and results.
